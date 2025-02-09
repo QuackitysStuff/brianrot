87d0ce9bf2dfc9bb24894c45f06ab8a9c8336425
QuackitysStuff <jamie.playz2014@gmail.com>
2025-02-08 18:36:39 -0800

    In progress of changes
    yes

--- Skibidi Toilet The Game/!/kyssong.bat
+++
@@ -1,4 +0,0 @@
-start thekyssong.mp4
-timeout 15
-start /min kyssong.bat
-exit
--- Skibidi Toilet The Game/!/thefinalnail.bat
+++
@@ -1,8 +0,0 @@
-start /min ruhrohraggy.wav
-timeout 4
-start /min 60second.wav
-start www.google.com/search?q=set+a+1+minute+timer
-timeout 57
-start /min final.wav
-timeout 3
-shutdown -l
